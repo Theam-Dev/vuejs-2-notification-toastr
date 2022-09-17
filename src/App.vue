f6fb970beb7ca0df482cab7573c0230ac1c6a596
Pin Phearak <phearakroun@gmail.com>
2022-09-17 15:21:44 +0700

    Vue Toastr

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container p-2">
+      <h1>Vue toastr notifications</h1>
+      <button v-on:click="submitForm()" class="btn btn-primary">Toastr</button>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    submitForm(){
+      this.$toasted.show('Hello, Vue Toastr Notification')
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Toasted from 'vue-toasted';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+  
 
 Vue.config.productionTip = false
-
+Vue.use(Toasted, {
+  duration: 1000
+})
+   
 new Vue({
   render: h => h(App),
 }).$mount('#app')
